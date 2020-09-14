e3296dd5d250af8e5c338e982cea0a33c19725fc
Samet Sökel <64185287+sametsoekel@users.noreply.github.com>
2020-09-14 03:32:55 +0300

    initial commits

--- R Scripts/preprocessing.R
+++ R Scripts/preprocessing.R
@@ -1,5 +1,10 @@
-train_x <- read_csv("train_x.csv")
+train_x <- read_csv("Data/train_x.csv")
 
-train_y <- read_csv("train_y.csv")
+train_y <- read_csv("Data/train_y.csv")
+
+
+###### SOME VARIABLES I FOUND INSIGNIFICANT GETS OUT -- 14/09/2020 #######
+
+train_xx <- train_x %>% select(-c("wpt_name","num_private","date_recorded","recorded_by"))
 
 
